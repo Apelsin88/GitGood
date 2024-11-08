1abce8d8c29862723be3f366cf52a310b541a7ae
Apelsin88 <143607654+Apelsin88@users.noreply.github.com>
2024-11-08 12:09:46 +0100

    :)

---
+++ RepoTraining/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RepoTraining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
